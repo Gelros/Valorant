e6c124f6651fa5591c7a82c533cbee4743cfdff3
ConstruisezUneAPI <guillaume.lacolley@gmail.com>
2024-02-27 16:24:10 +0100

    card-agent

--- src/components/Propriete.jsx
+++ src/components/Propriete.jsx
@@ -1,62 +1,118 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Data from "../data/persos.json";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faChevronLeft} from "@fortawesome/free-solid-svg-icons"
-import {Link} from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Propriete = () => {
-    const id = window.location.href
-    let url = id.substring(38);
-    const data = Data.find((data) => data.uuid === url)
-    console.log(data);
+  const id = window.location.href;
+  let url = id.substring(38);
+  const data = Data.find((data) => data.uuid === url);
+  console.log(data);
 
-    const [item, setItem] = useState(0)
+  const [item, setItem] = useState(0);
 
-    
-    
-    console.log(item);
-        return (
-            <div className='property'>
-                
-                <Link to={"/agents"}><FontAwesomeIcon className='icon-retour' icon={faChevronLeft} /></Link>
-                
-                {
-                    
-                        <div className="card-details">
+  console.log(item);
+  return (
+    <div className="property">
+      <Link to={"/agents"}>
+        <FontAwesomeIcon className="icon-retour" icon={faChevronLeft} />
+      </Link>
 
-                            <div className="description">
-                               <img src={data.bustPortrait} alt={`${data.displayName} portrait`} /> 
-                               <div className="text">
-                                <h1 className="color2">{data.displayName}</h1>
-                                <div className="bio">
-                                    <h2>Biographie</h2>
-                                    <p>{data.description}</p> 
-                                 </div>                                                             
-                               </div>
-                            </div>
-
-                            <div className="spells">
-                                <div className="spell c-1">
-                                    <h2 className="spell-name">{data.abilities[item].displayName}</h2>
-                                    <div className="spell-desc">{data.abilities[item].description}</div>
-                                </div>
-                                <div className="illustration">
-                                    <div className="video"></div>
-                                    <div className="spell-choices">
-                                       <button onClick={() => setItem(0)} className={`ab-1 ${item === 0 ? "bg" : ""}`} value="1"><span>1</span></button>
-                                       <button onClick={() => setItem(1)} className={`ab-2 ${item === 1 ? "bg" : ""}`} value="2"><span>2</span></button>
-                                       <button onClick={() => setItem(2)} className={`ab-3 ${item === 2 ? "bg" : ""}`} value="3"><span>3</span></button>
-                                       <button onClick={() => setItem(3)} className={`ab-4 ${item === 3 ? "bg" : ""}`} value="4"><span>4</span></button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    
-                }
+      {
+        <div className="card-details">
+          <div className="description">
+            <div className="artwork">
+                <div className="name">
+                    <span>{data.displayName}</span>
+                    <span>{data.displayName}</span>
+                </div>
+                <img src={data.bustPortrait} alt={`${data.displayName} portrait`} />
             </div>
-        );
-    
-    
+            
+            <div className="text">
+              <h1 className="color2">{data.displayName}</h1>
+              <div className="bio">
+                <h2>Biographie</h2>
+                <p>{data.description}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="spells">
+            <div className="spell c-1">
+              <h2 className="spell-name">{data.abilities[item].displayName}</h2>
+              <div className="spell-desc">
+                {data.abilities[item].description}
+              </div>
+            </div>
+            <div className="illustration">
+              <div className="video">
+                <video
+                  src={data.abilities[0].vid}
+                  autoPlay
+                  muted
+                  loop
+                  className={` ${item === 0 ? "active" : "non-active"} `}
+                ></video>
+                <video
+                  src={data.abilities[1].vid}
+                  autoPlay
+                  muted
+                  loop
+                  className={` ${item === 1 ? "active" : "non-active"} `}
+                ></video>
+                <video
+                  src={data.abilities[2].vid}
+                  autoPlay
+                  muted
+                  loop
+                  className={` ${item === 2 ? "active" : "non-active"} `}
+                ></video>
+                <video
+                  src={data.abilities[3].vid}
+                  autoPlay
+                  muted
+                  loop
+                  className={` ${item === 3 ? "active" : "non-active"} `}
+                ></video>
+              </div>
+              <div className="spell-choices">
+                <button
+                  onClick={() => setItem(0)}
+                  className={`ab-1 ${item === 0 ? "bg" : ""}`}
+                  value="1"
+                >
+                  <span>1</span>
+                </button>
+                <button
+                  onClick={() => setItem(1)}
+                  className={`ab-2 ${item === 1 ? "bg" : ""}`}
+                  value="2"
+                >
+                  <span>2</span>
+                </button>
+                <button
+                  onClick={() => setItem(2)}
+                  className={`ab-3 ${item === 2 ? "bg" : ""}`}
+                  value="3"
+                >
+                  <span>3</span>
+                </button>
+                <button
+                  onClick={() => setItem(3)}
+                  className={`ab-4 ${item === 3 ? "bg" : ""}`}
+                  value="4"
+                >
+                  <span>4</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      }
+    </div>
+  );
 };
 
 export default Propriete;
