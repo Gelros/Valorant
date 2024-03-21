308775c96a5981c25040de62a2c9ec724c5362d4
Gelros <100789293+Gelros@users.noreply.github.com>
2024-03-21 20:22:08 +0000

    a

--- src/components/ArmCard.jsx
+++ src/components/ArmCard.jsx
@@ -1,8 +1,25 @@
 import {React, useState} from 'react';
 import Slider from "react-slick"
+import {FaArrowRight, FaArrowLeft} from "react-icons/fa"
 
 const ArmCard = ({item}) => {
     console.log(...item);
+
+    const NextArrow = ({onClick}) => {
+        return (
+            <div className="arrow next" onClick={onClick}>
+              <FaArrowRight/>
+            </div>
+        )
+    }
+
+    const PrevArrow = ({onClick}) => {
+        return (
+            <div className="arrow prev" onClick={onClick}>
+              <FaArrowLeft/>
+            </div>
+        )
+    }
 
     const settings = {
         infinite: true,
@@ -10,12 +27,14 @@ const ArmCard = ({item}) => {
         speed:300,
         slidesToShow:3,
         centerMode:true,
-        centerPadding:0
+        centerPadding:0,
+        nextArrow : <NextArrow/>,
+        prevArrow: <PrevArrow/>
     }
     
     return (
         <div className='slide-weapon'>
-            <Slider>
+            <Slider {...settings}>
                 {
                     item.map((e, idx) => (
                         <div>
